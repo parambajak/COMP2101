719c468f8145b8a1e8f76903b0fcbea66223ae00
Parampreet singh <comp2101-01@zonzorp.net>
2020-01-14 13:31:36 -0800

    lab1

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,2 +1,5 @@
 #!/bin/#!/usr/bin/env bash
+# My first script - helloworld.sh
+
 echo "hello World!"
+echo "I am process # $$ "
---
+++ bash/helloworldtemplated.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# My Second script - helloworldtemplated.sh
+
+cat <<EOF
+Hello world!
+I am process # $$
+EOF
+
