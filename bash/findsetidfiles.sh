ed676774d280c8e6b0299d68640abb80c0a1be31
Parampreet singh <comp2101-01@zonzorp.net>
2020-01-22 17:49:49 -0800

    file updated final lab 1

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -23,3 +23,13 @@ echo "Setuid files:"
 echo "============="
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 echo ""
+
+echo "Setgit files:"
+echo "============="
+find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 5
+echo ""
+
+echo "Setuid files:"
+echo "============="
+find / -type f -exec du -h {} + 2>/dev/null | sort -h -r | head 10
+echo ""
